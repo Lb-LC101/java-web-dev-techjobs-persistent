78fee2f06c668f0383f7b43327ebae334c5ceb2e
Lb-LC101 <lisabartfeld@gmail.com>
2021-03-12 11:38:47 -0600

    Completed Part 2 Models

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,32 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    // Class variables
+    @NotBlank(message = "Location is required")
+    @Size(min = 5, max = 50, message = "Location must be between 5 and 50 characters")
+    private String location;
+
+    // Constructors
+    public Employer() {}
+
+    // Methods
+
+    //getters and setters
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
 
 
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,29 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    // Class variables
+    @NotBlank(message = "Name is required")
+    @Size(min = 5, max = 255, message = "Skill description must be between 5 and 255 characters")
+    private String skills;
+
+    // Constructors
+    public Skill() {}
+
+    // Methods
+
+    //getters and setters
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public void setSkills(String skills) {
+        this.skills = skills;
+    }
 }
